c37f83a7522d40b0cab3b9d05d115a08cbf1f0aa
Igor Bubelov <ibubelov@gmail.com>
2015-10-09 12:04:02 +0700

    Added tests for exchange rate services

---
+++ coins/src/test/java/ExchangeRatesIntegrationTest.java
@@ -0,0 +1,56 @@
+import com.bubelov.coins.BuildConfig;
+import com.bubelov.coins.model.Currency;
+import com.bubelov.coins.model.ExchangeRate;
+import com.bubelov.coins.service.rates.provider.ExchangeRatesProviderFactory;
+import com.bubelov.coins.service.rates.provider.ExchangeRatesProviderType;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.RobolectricGradleTestRunner;
+import org.robolectric.RuntimeEnvironment;
+import org.robolectric.annotation.Config;
+
+/**
+ * Author: Igor Bubelov
+ * Date: 10/9/15 11:52 AM
+ */
+
+@RunWith(RobolectricGradleTestRunner.class)
+@Config(constants = BuildConfig.class, sdk = 21)
+public class ExchangeRatesIntegrationTest {
+    @Test
+    public void testBitstamp() {
+        testProvider(ExchangeRatesProviderType.BITSTAMP);
+    }
+
+    @Test
+    public void testCoinbase() {
+        testProvider(ExchangeRatesProviderType.COINBASE);
+    }
+
+    @Test
+    public void testBitcoinAverage() {
+        testProvider(ExchangeRatesProviderType.BITCOIN_AVERAGE);
+    }
+
+    @Test
+    public void testWinkdex() {
+        testProvider(ExchangeRatesProviderType.WINKDEX);
+    }
+
+    private void testProvider(ExchangeRatesProviderType providerType) {
+        Currency btc = Currency.query(RuntimeEnvironment.application, "BTC");
+        Currency usd = Currency.query(RuntimeEnvironment.application, "USD");
+
+        ExchangeRate exchangeRate = null;
+
+        try {
+            exchangeRate = ExchangeRatesProviderFactory.newProvider(providerType).getExchangeRate(btc, usd);
+        } catch (Exception ignored) {
+
+        }
+
+        Assert.assertTrue(exchangeRate != null && exchangeRate.getValue() > 0);
+    }
+}
