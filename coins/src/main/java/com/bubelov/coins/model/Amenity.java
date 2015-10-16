a028b0f8d8edf25510a4462cae685767e22b75cf
Igor Bubelov <ibubelov@gmail.com>
2015-10-16 23:51:52 +0700

    Added icons to merchants search

--- coins/src/main/java/com/bubelov/coins/model/Amenity.java
+++ coins/src/main/java/com/bubelov/coins/model/Amenity.java
@@ -1,5 +1,7 @@
 package com.bubelov.coins.model;
 
+import com.bubelov.coins.R;
+
 /**
  * Author: Igor Bubelov
  * Date: 21/05/15 12:40
@@ -19,5 +21,40 @@ public enum Amenity {
     PARKING,
     PHARMACY,
     PIZZA,
-    TAXI
+    TAXI;
+
+    public int getIconId() {
+        switch (this) {
+            case ATM:
+                return R.drawable.ic_atm;
+            case CAFE:
+                return R.drawable.ic_cafe;
+            case RESTAURANT:
+                return R.drawable.ic_restaurant;
+            case BAR:
+                return R.drawable.ic_bar;
+            case HOTEL:
+                return R.drawable.ic_hotel;
+            case CAR_WASH:
+                return R.drawable.ic_car_wash;
+            case FUEL:
+                return R.drawable.ic_gas_station;
+            case HOSPITAL:
+                return R.drawable.ic_hospital;
+            case DRY_CLEANING:
+                return R.drawable.ic_laundry;
+            case CINEMA:
+                return R.drawable.ic_movies;
+            case PARKING:
+                return R.drawable.ic_parking;
+            case PHARMACY:
+                return R.drawable.ic_pharmacy;
+            case PIZZA:
+                return R.drawable.ic_pizza;
+            case TAXI:
+                return R.drawable.ic_taxi;
+            default:
+                return R.drawable.ic_place_24dp;
+        }
+    }
 }
