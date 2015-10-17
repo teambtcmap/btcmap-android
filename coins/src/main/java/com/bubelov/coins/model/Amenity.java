fad0194127f8be0e83867f40f355d39af52d9a6d
Igor Bubelov <ibubelov@gmail.com>
2015-10-17 15:05:20 +0700

    Vector icons for navigation drawer

--- coins/src/main/java/com/bubelov/coins/model/Amenity.java
+++ coins/src/main/java/com/bubelov/coins/model/Amenity.java
@@ -26,33 +26,33 @@ public enum Amenity {
     public int getIconId() {
         switch (this) {
             case ATM:
-                return R.drawable.ic_atm;
+                return R.drawable.ic_atm_24dp;
             case CAFE:
-                return R.drawable.ic_cafe;
+                return R.drawable.ic_cafe_24dp;
             case RESTAURANT:
-                return R.drawable.ic_restaurant;
+                return R.drawable.ic_restaurant_24dp;
             case BAR:
-                return R.drawable.ic_bar;
+                return R.drawable.ic_bar_24dp;
             case HOTEL:
-                return R.drawable.ic_hotel;
+                return R.drawable.ic_hotel_24dp;
             case CAR_WASH:
-                return R.drawable.ic_car_wash;
+                return R.drawable.ic_car_wash_24dp;
             case FUEL:
-                return R.drawable.ic_gas_station;
+                return R.drawable.ic_gas_station_24dp;
             case HOSPITAL:
-                return R.drawable.ic_hospital;
+                return R.drawable.ic_hospital_24dp;
             case DRY_CLEANING:
                 return R.drawable.ic_laundry;
             case CINEMA:
-                return R.drawable.ic_movies;
+                return R.drawable.ic_cinema_24dp;
             case PARKING:
-                return R.drawable.ic_parking;
+                return R.drawable.ic_parking_24dp;
             case PHARMACY:
-                return R.drawable.ic_pharmacy;
+                return R.drawable.ic_pharmacy_24dp;
             case PIZZA:
-                return R.drawable.ic_pizza;
+                return R.drawable.ic_pizza_24dp;
             case TAXI:
-                return R.drawable.ic_taxi;
+                return R.drawable.ic_taxi_24dp;
             default:
                 return R.drawable.ic_place_24dp;
         }
