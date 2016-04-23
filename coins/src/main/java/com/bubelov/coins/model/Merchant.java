f6e4fb20f702d73ac5ceffb2bca47eec4b97dc9f
Igor Bubelov <ibubelov@gmail.com>
2016-04-23 15:44:31 +0700

    Continued working on database refactoring

--- coins/src/main/java/com/bubelov/coins/model/Merchant.java
+++ coins/src/main/java/com/bubelov/coins/model/Merchant.java
@@ -2,6 +2,7 @@ package com.bubelov.coins.model;
 
 import android.database.DatabaseUtils;
 import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteStatement;
 
 import com.bubelov.coins.dagger.Injector;
 import com.bubelov.coins.database.DbContract;
@@ -9,6 +10,9 @@ import com.google.android.gms.maps.model.LatLng;
 import com.google.maps.android.clustering.ClusterItem;
 
 import java.util.Collection;
+import java.util.List;
+
+import timber.log.Timber;
 
 /**
  * Author: Igor Bubelov
@@ -135,5 +139,77 @@ public class Merchant extends AbstractEntity implements ClusterItem {
 
     public void setCurrencies(Collection<Currency> currencies) {
         this.currencies = currencies;
+    }
+
+    // Database stuff
+
+    public static void insert(List<Merchant> merchants) {
+        SQLiteDatabase db = Injector.INSTANCE.getAppComponent().database();
+        db.beginTransaction();
+
+        try {
+            insertMerchants(merchants);
+            insertCurrenciesMerchants(merchants);
+            db.setTransactionSuccessful();
+        } catch (Exception e) {
+            Timber.e(e, "Failed to insert merchants");
+        } finally {
+            db.endTransaction();
+        }
+    }
+
+    private static void insertMerchants(List<Merchant> merchants) {
+        SQLiteDatabase db = Injector.INSTANCE.getAppComponent().database();
+
+        String insertQuery = String.format("insert or replace into %s (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s) values (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)",
+                DbContract.Merchants.TABLE_NAME,
+                DbContract.Merchants._ID,
+                DbContract.Merchants._CREATED_AT,
+                DbContract.Merchants._UPDATED_AT,
+                DbContract.Merchants.LATITUDE,
+                DbContract.Merchants.LONGITUDE,
+                DbContract.Merchants.NAME,
+                DbContract.Merchants.DESCRIPTION,
+                DbContract.Merchants.PHONE,
+                DbContract.Merchants.WEBSITE,
+                DbContract.Merchants.AMENITY,
+                DbContract.Merchants.OPENING_HOURS,
+                DbContract.Merchants.ADDRESS);
+
+        SQLiteStatement insertStatement = db.compileStatement(insertQuery);
+
+        for (Merchant merchant : merchants) {
+            insertStatement.bindLong(1, merchant.getId());
+            insertStatement.bindLong(2, merchant.getCreatedAt().getMillis());
+            insertStatement.bindLong(3, merchant.getUpdatedAt().getMillis());
+            insertStatement.bindDouble(4, merchant.getLatitude());
+            insertStatement.bindDouble(5, merchant.getLongitude());
+            insertStatement.bindString(6, getEmptyStringIfNull(merchant.getName()));
+            insertStatement.bindString(7, getEmptyStringIfNull(merchant.getDescription()));
+            insertStatement.bindString(8, getEmptyStringIfNull(merchant.getPhone()));
+            insertStatement.bindString(9, getEmptyStringIfNull(merchant.getWebsite()));
+            insertStatement.bindString(10, getEmptyStringIfNull(merchant.getAmenity()));
+            insertStatement.bindString(11, getEmptyStringIfNull(merchant.getOpeningHours()));
+            insertStatement.bindString(12, getEmptyStringIfNull(merchant.getAddress()));
+            insertStatement.execute();
+        }
+    }
+
+    private static void insertCurrenciesMerchants(List<Merchant> merchants) {
+        SQLiteDatabase db = Injector.INSTANCE.getAppComponent().database();
+        String insertQuery = "insert or replace into currencies_merchants (currency_id, merchant_id) values (?, ?)";
+        SQLiteStatement insertStatement = db.compileStatement(insertQuery);
+
+        for (Merchant merchant : merchants) {
+            for (Currency currency : merchant.getCurrencies()) {
+                insertStatement.bindLong(1, currency.getId());
+                insertStatement.bindLong(2, merchant.getId());
+                insertStatement.execute();
+            }
+        }
+    }
+
+    private static String getEmptyStringIfNull(String string) {
+        return string == null ? "" : string;
     }
 }
