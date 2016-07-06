92bc1f12a65c7c2ab8d2c8ba23b2f10c73257de8
Igor Bubelov <ibubelov@gmail.com>
2016-07-06 23:05:54 +0700

    Refactor currencies filter

--- app/src/main/java/com/bubelov/coins/model/Currency.java
+++ app/src/main/java/com/bubelov/coins/model/Currency.java
@@ -2,7 +2,10 @@ package com.bubelov.coins.model;
 
 import android.content.ContentValues;
 import android.database.Cursor;
+import android.database.DatabaseUtils;
 import android.database.sqlite.SQLiteDatabase;
+import android.provider.BaseColumns;
+import android.support.annotation.NonNull;
 
 import com.bubelov.coins.dagger.Injector;
 import com.bubelov.coins.database.DbContract;
@@ -26,6 +29,8 @@ public class Currency extends AbstractEntity implements Serializable {
     private String code;
 
     private boolean crypto;
+
+    private boolean showOnMap = true;
 
     public String getName() {
         return name;
@@ -51,14 +56,27 @@ public class Currency extends AbstractEntity implements Serializable {
         this.crypto = crypto;
     }
 
+    public boolean isShowOnMap() {
+        return showOnMap;
+    }
+
+    public void setShowOnMap(boolean showOnMap) {
+        this.showOnMap = showOnMap;
+    }
+
     // Database stuff
+
+    public static long getCount() {
+        SQLiteDatabase db = Injector.INSTANCE.getAppComponent().database();
+        return DatabaseUtils.queryNumEntries(db, DbContract.Currencies.TABLE_NAME);
+    }
 
     public static Currency find(long id) {
         SQLiteDatabase db = Injector.INSTANCE.getAppComponent().database();
         Cursor cursor = db.query(DbContract.Currencies.TABLE_NAME, null, "_id = ?", new String[]{String.valueOf(id)}, null, null, null);
 
         try {
-            return cursor.moveToNext() ? fromCursor(cursor) : null;
+            return fromCursor(cursor).get(0);
         } finally {
             cursor.close();
         }
@@ -69,10 +87,24 @@ public class Currency extends AbstractEntity implements Serializable {
         Cursor cursor = db.query(DbContract.Currencies.TABLE_NAME, null, "code = ?", new String[]{code}, null, null, null);
 
         try {
-            return cursor.moveToNext() ? fromCursor(cursor) : null;
+            return fromCursor(cursor).get(0);
         } finally {
             cursor.close();
         }
+    }
+
+    public static List<Currency> find(boolean fiat) {
+        SQLiteDatabase db = Injector.INSTANCE.getAppComponent().database();
+
+        return fromCursor(db.query(
+                DbContract.Currencies.TABLE_NAME,
+                null,
+                String.format("%s = ?", DbContract.Currencies.CRYPTO),
+                new String[]{fiat ? "0" : "1"},
+                null,
+                null,
+                BaseColumns._ID + " asc"
+        ));
     }
 
     public static List<Currency> findByMerchant(Merchant merchant) {
@@ -123,22 +155,26 @@ public class Currency extends AbstractEntity implements Serializable {
         values.put(DbContract.Currencies.NAME, getName());
         values.put(DbContract.Currencies.CODE, getCode());
         values.put(DbContract.Currencies.CRYPTO, isCrypto());
+        values.put(DbContract.Currencies.SHOW_ON_MAP, isShowOnMap());
 
         return values;
     }
 
-    private static Currency fromCursor(Cursor cursor) {
-        if (cursor == null) {
-            return null;
+    private static List<Currency> fromCursor(@NonNull Cursor cursor) {
+        List<Currency> currencies = new ArrayList<>();
+
+        while (cursor.moveToNext()) {
+            Currency currency = new Currency();
+            currency.setId(cursor.getLong(cursor.getColumnIndex(DbContract.Currencies._ID)));
+            currency.setName(cursor.getString(cursor.getColumnIndex(DbContract.Currencies.NAME)));
+            currency.setCode(cursor.getString(cursor.getColumnIndex(DbContract.Currencies.CODE)));
+            currency.setCrypto(cursor.getInt(cursor.getColumnIndex(DbContract.Currencies.CRYPTO)) == 1);
+            currency.setShowOnMap(cursor.getInt(cursor.getColumnIndex(DbContract.Currencies.SHOW_ON_MAP)) == 1);
+            currency.setCreatedAt(new DateTime(cursor.getLong(cursor.getColumnIndex(DbContract.Currencies._CREATED_AT))));
+            currency.setUpdatedAt(new DateTime(cursor.getLong(cursor.getColumnIndex(DbContract.Currencies._UPDATED_AT))));
+            currencies.add(currency);
         }
 
-        Currency currency = new Currency();
-        currency.setId(cursor.getLong(cursor.getColumnIndex(DbContract.Currencies._ID)));
-        currency.setName(cursor.getString(cursor.getColumnIndex(DbContract.Currencies.NAME)));
-        currency.setCode(cursor.getString(cursor.getColumnIndex(DbContract.Currencies.CODE)));
-        currency.setCrypto(cursor.getInt(cursor.getColumnIndex(DbContract.Currencies.CRYPTO)) == 1);
-        currency.setCreatedAt(new DateTime(cursor.getLong(cursor.getColumnIndex(DbContract.Currencies._CREATED_AT))));
-        currency.setUpdatedAt(new DateTime(cursor.getLong(cursor.getColumnIndex(DbContract.Currencies._UPDATED_AT))));
-        return currency;
+        return currencies;
     }
 }
