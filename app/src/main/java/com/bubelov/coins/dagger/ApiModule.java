bd7669ff4b276aa60bfd383e4f86795fd59654d2
Igor Bubelov <ibubelov@gmail.com>
2016-12-10 18:08:07 +0700

    Add HTTP logging support

--- app/src/main/java/com/bubelov/coins/dagger/ApiModule.java
+++ app/src/main/java/com/bubelov/coins/dagger/ApiModule.java
@@ -6,10 +6,14 @@ import com.bubelov.coins.R;
 import com.bubelov.coins.api.CoinsApi;
 import com.google.gson.Gson;
 
+import java.util.concurrent.TimeUnit;
+
 import javax.inject.Singleton;
 
 import dagger.Module;
 import dagger.Provides;
+import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
@@ -22,9 +26,20 @@ import retrofit2.converter.gson.GsonConverterFactory;
 public class ApiModule {
     @Provides @Singleton
     CoinsApi provideApi(Gson gson, Context context) {
+        HttpLoggingInterceptor logging = new HttpLoggingInterceptor();
+        logging.setLevel(HttpLoggingInterceptor.Level.NONE);
+
+        OkHttpClient client = new OkHttpClient.Builder()
+                .connectTimeout(10, TimeUnit.SECONDS)
+                .readTimeout(30, TimeUnit.SECONDS)
+                .writeTimeout(30, TimeUnit.SECONDS)
+                .addInterceptor(logging)
+                .build();
+
         return new Retrofit.Builder()
                 .baseUrl(context.getString(R.string.api_url))
                 .addConverterFactory(GsonConverterFactory.create(gson))
+                .client(client)
                 .build()
                 .create(CoinsApi.class);
     }
