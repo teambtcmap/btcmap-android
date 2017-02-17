c123343b3413069c9dcb352ee1831b7045380716
Igor Bubelov <ibubelov@gmail.com>
2017-02-17 21:58:09 +0700

    Show exchange rates

--- app/src/main/java/com/bubelov/coins/ui/activity/ExchangeRatesActivity.java
+++ app/src/main/java/com/bubelov/coins/ui/activity/ExchangeRatesActivity.java
@@ -1,5 +1,6 @@
 package com.bubelov.coins.ui.activity;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.util.Pair;
@@ -9,11 +10,18 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 
 import com.bubelov.coins.R;
+import com.bubelov.coins.api.rates.provider.BitcoinAverage;
+import com.bubelov.coins.api.rates.provider.Bitstamp;
+import com.bubelov.coins.api.rates.provider.Coinbase;
 import com.bubelov.coins.api.rates.provider.CryptoExchange;
+import com.bubelov.coins.api.rates.provider.Winkdex;
 import com.bubelov.coins.ui.adapter.ExchangeRatesAdapter;
+import com.crashlytics.android.Crashlytics;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -29,9 +37,11 @@ public class ExchangeRatesActivity extends AbstractActivity {
     @BindView(R.id.list)
     RecyclerView ratesView;
 
-    ExchangeRatesAdapter ratesAdapter;
+    private ExchangeRatesAdapter ratesAdapter;
 
-    List<Pair<CryptoExchange, Double>> ratesList = new ArrayList<>();
+    private List<CryptoExchange> exchanges = new ArrayList<>();
+
+    private Map<CryptoExchange, Double> rates = new HashMap<>();
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -49,7 +59,43 @@ public class ExchangeRatesActivity extends AbstractActivity {
         ratesView.setLayoutManager(new LinearLayoutManager(this));
         ratesView.setHasFixedSize(true);
 
-        ratesAdapter = new ExchangeRatesAdapter(ratesList);
+        exchanges.add(new Bitstamp());
+        exchanges.add(new Coinbase());
+        exchanges.add(new Winkdex());
+        exchanges.add(new BitcoinAverage());
+
+        ratesAdapter = new ExchangeRatesAdapter(exchanges, rates);
         ratesView.setAdapter(ratesAdapter);
+
+        new LoadExchangeRatesTask().execute();
+    }
+
+    private class LoadExchangeRatesTask extends AsyncTask<Void, Pair<CryptoExchange, Double>, Void> {
+        @Override
+        protected void onPreExecute() {
+            rates.clear();
+            ratesAdapter.notifyDataSetChanged();
+        }
+
+        @Override
+        protected Void doInBackground(Void... voids) {
+            for (CryptoExchange exchange : exchanges) {
+                try {
+                    publishProgress(new Pair<>(exchange, exchange.getCurrentRate()));
+                } catch (Exception e) {
+                    Crashlytics.logException(e);
+                }
+            }
+
+            return null;
+        }
+
+        @Override
+        protected void onProgressUpdate(Pair<CryptoExchange, Double>... values) {
+            for (Pair<CryptoExchange, Double> rate : values) {
+                rates.put(rate.first, rate.second);
+                ratesAdapter.notifyDataSetChanged();
+            }
+        }
     }
 }
--- app/src/main/java/com/bubelov/coins/ui/adapter/ExchangeRatesAdapter.java
+++ app/src/main/java/com/bubelov/coins/ui/adapter/ExchangeRatesAdapter.java
@@ -1,14 +1,20 @@
 package com.bubelov.coins.ui.adapter;
 
-import android.support.v4.util.Pair;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.ViewSwitcher;
 
+import com.bubelov.coins.R;
 import com.bubelov.coins.api.rates.provider.CryptoExchange;
 
 import java.util.List;
+import java.util.Locale;
+import java.util.Map;
 
+import butterknife.BindView;
 import butterknife.ButterKnife;
 
 /**
@@ -16,28 +22,53 @@ import butterknife.ButterKnife;
  */
 
 public class ExchangeRatesAdapter extends RecyclerView.Adapter<ExchangeRatesAdapter.ExchangeRateViewHolder> {
-    private List<Pair<CryptoExchange, Double>> items;
+    private final List<CryptoExchange> exchanges;
 
-    public ExchangeRatesAdapter(List<Pair<CryptoExchange, Double>> items) {
-        this.items = items;
+    private final Map<CryptoExchange, Double> rates;
+
+    public ExchangeRatesAdapter(List<CryptoExchange> exchanges, Map<CryptoExchange, Double> rates) {
+        this.exchanges = exchanges;
+        this.rates = rates;
     }
 
     @Override
     public ExchangeRateViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        return null;
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.list_item_exchange_rate, parent, false);
+        return new ExchangeRateViewHolder(view);
     }
 
     @Override
     public void onBindViewHolder(final ExchangeRateViewHolder holder, int position) {
-        // TODO
+        CryptoExchange exchange = exchanges.get(position);
+        holder.exchangeName.setText(exchange.getClass().getSimpleName());
+        double price = rates.containsKey(exchange) ? rates.get(exchange) : 0;
+
+        if (price == 0 && holder.priceSwitcher.getDisplayedChild() == 1) {
+            holder.priceSwitcher.setDisplayedChild(0);
+        }
+
+        if (price > 0 && holder.priceSwitcher.getDisplayedChild() == 0) {
+            holder.priceSwitcher.setDisplayedChild(1);
+        }
+
+        holder.price.setText(String.format(Locale.US, "$%.2f", price));
     }
 
     @Override
     public int getItemCount() {
-        return items.size();
+        return exchanges.size();
     }
 
     public static class ExchangeRateViewHolder extends RecyclerView.ViewHolder {
+        @BindView(R.id.exchange_name)
+        TextView exchangeName;
+
+        @BindView(R.id.price_flipper)
+        ViewSwitcher priceSwitcher;
+
+        @BindView(R.id.price)
+        TextView price;
+
         public ExchangeRateViewHolder(View itemView) {
             super(itemView);
             ButterKnife.bind(this, itemView);
