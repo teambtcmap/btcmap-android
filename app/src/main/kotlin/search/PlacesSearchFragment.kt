2823442e7b2e0c55449f4014a30103f7d0d73357
Ebrahim Byagowi <ebrahim@gnu.org>
2022-05-18 17:03:06 +0430

    Simplify search text change listener logic
    It uses two ltx extensions, doAfterTextChanged and isVisible to simplify logic of the listener.

--- app/src/main/kotlin/search/PlacesSearchFragment.kt
+++ app/src/main/kotlin/search/PlacesSearchFragment.kt
@@ -3,12 +3,13 @@ package search
 import android.os.Bundle
 import android.text.Editable
 import androidx.recyclerview.widget.LinearLayoutManager
-import android.text.TextUtils
 import android.text.TextWatcher
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.view.inputmethod.InputMethodManager
+import androidx.core.view.isVisible
+import androidx.core.widget.doAfterTextChanged
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
@@ -75,12 +76,11 @@ class PlacesSearchFragment : Fragment() {
 
         binding.query.requestFocus()
 
-        binding.query.addTextChangedListener(object : TextWatcherAdapter() {
-            override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) {
-                model.setSearchString(s.toString())
-                binding.clear.visibility = if (TextUtils.isEmpty(s)) View.GONE else View.VISIBLE
-            }
-        })
+        binding.query.doAfterTextChanged {
+            val text = it.toString()
+            model.setSearchString(text)
+            binding.clear.isVisible = text.isNotEmpty()
+        }
 
         binding.clear.setOnClickListener { binding.query.setText("") }
     }
@@ -88,11 +88,5 @@ class PlacesSearchFragment : Fragment() {
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
-    }
-
-    abstract class TextWatcherAdapter : TextWatcher {
-        override fun beforeTextChanged(s: CharSequence, start: Int, count: Int, after: Int) {}
-        override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) {}
-        override fun afterTextChanged(s: Editable) {}
     }
 }
