d0a43950db754cff15aefec2cff016f0b631a532
Igor Bubelov <igor@bubelov.com>
2022-05-23 17:49:00 +0700

    Update deps

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -108,7 +108,7 @@ dependencies {
     val navVer = "2.4.2"
     implementation("androidx.navigation:navigation-fragment-ktx:$navVer")
     implementation("androidx.navigation:navigation-ui-ktx:$navVer")
-    implementation("androidx.constraintlayout:constraintlayout:2.1.3")
+    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation("androidx.preference:preference:1.2.0")
     implementation("com.google.android.material:material:1.6.0")
 
