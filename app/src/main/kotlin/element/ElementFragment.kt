9177f685836baa8c93901c87c26dc9be526b7acc
Igor Bubelov <igor@bubelov.com>
2022-05-27 16:28:51 +0700

    Add element actions

--- app/src/main/kotlin/element/ElementFragment.kt
+++ app/src/main/kotlin/element/ElementFragment.kt
@@ -1,5 +1,7 @@
 package element
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -24,12 +26,33 @@ class ElementFragment : Fragment() {
         return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        binding.toolbar.setOnMenuItemClickListener {
+            when (it.itemId) {
+                R.id.action_edit,
+                R.id.action_delete -> {
+                    val intent = Intent(Intent.ACTION_VIEW)
+                    intent.data = Uri.parse("https://wiki.openstreetmap.org/wiki/How_to_contribute")
+                    startActivity(intent)
+                }
+            }
+
+            true
+        }
+    }
+
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
     }
 
     fun setScrollProgress(progress: Float) {
+        val edit = binding.toolbar.menu.findItem(R.id.action_edit)!!
+        edit.isVisible = progress == 1.0f
+
+        val delete = binding.toolbar.menu.findItem(R.id.action_delete)!!
+        delete.isVisible = progress == 1.0f
+
         binding.divider.alpha = 0.12f * progress
     }
 
@@ -37,7 +60,8 @@ class ElementFragment : Fragment() {
         binding.toolbar.title = element.tags["name"]?.jsonPrimitive?.content ?: "Unnamed"
         binding.phone.text = element.tags["phone"]?.jsonPrimitive?.content ?: getString(R.string.not_provided)
         binding.website.text = element.tags["website"]?.jsonPrimitive?.content ?: getString(R.string.not_provided)
-        binding.openingHours.text = element.tags["opening_hours"]?.jsonPrimitive?.content ?: getString(R.string.not_provided)
+        binding.openingHours.text =
+            element.tags["opening_hours"]?.jsonPrimitive?.content ?: getString(R.string.not_provided)
         binding.tags.text = element.tags.toString()
     }
 }
