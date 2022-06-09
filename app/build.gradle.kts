0ccd3a52747b3a65af7a81dde193499cd58f0554
Igor Bubelov <igor@bubelov.com>
2022-06-09 15:02:13 +0700

    Bump version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -20,8 +20,8 @@ android {
         applicationId = "org.btcmap"
         minSdk = 26
         targetSdk = 31
-        versionCode = 2
-        versionName = "0.2.0"
+        versionCode = 3
+        versionName = "0.3"
         setProperty("archivesBaseName", "btcmap-$versionName")
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
