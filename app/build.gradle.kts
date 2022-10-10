afecde4e6e6201a3dcfd737c5b03f750c1637fc1
Igor Bubelov <igor@bubelov.com>
2022-10-10 23:27:55 +0700

    Fix lint issue

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -189,7 +189,7 @@ dependencies {
     implementation("com.github.PhilJay:MPAndroidChart:v3.1.0")
 
     // Used to download, cache and display images
-    implementation("com.squareup.picasso:picasso:2.71828")
+    implementation("com.squareup.picasso:picasso:2.8")
 
     // Common test dependencies
     testImplementation("junit:junit:4.13.2")
