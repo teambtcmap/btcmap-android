ecbb0d35b8b490eaf2d9d71f4c1b2117f7c69fc4
Igor Bubelov <igor@bubelov.com>
2023-04-12 15:54:06 +0700

    Improve test coverage

--- app/src/androidTest/kotlin/activity/ActivityTest.kt
+++ app/src/androidTest/kotlin/activity/ActivityTest.kt
@@ -4,6 +4,7 @@ import androidx.test.core.app.launchActivity
 import androidx.test.espresso.Espresso
 import androidx.test.espresso.assertion.ViewAssertions.*
 import androidx.test.espresso.matcher.ViewMatchers.*
+import junit.framework.TestCase.assertFalse
 import org.btcmap.R
 import org.hamcrest.Matchers.allOf
 import org.junit.Test
@@ -12,10 +13,14 @@ class ActivityTest {
 
     @Test
     fun launch() {
-        launchActivity<Activity>().use {
+        launchActivity<Activity>().use { scenario ->
             Espresso
                 .onView(allOf(withId(R.id.nav_host_fragment), withParent(withId(-1))))
                 .check(matches(isDisplayed()))
+
+            scenario.onActivity {
+                assertFalse(it.window.decorView.fitsSystemWindows)
+            }
         }
     }
 }
