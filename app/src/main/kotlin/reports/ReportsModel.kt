83b82e25e0c2405346b8d333073fbff330ccdfb5
Igor Bubelov <igor@bubelov.com>
2024-02-05 11:13:15 +0700

    Fix crash during chart data generation

--- app/src/main/kotlin/reports/ReportsModel.kt
+++ app/src/main/kotlin/reports/ReportsModel.kt
@@ -52,26 +52,29 @@ class ReportsModel(
                                     }
                                 },
                                 verifiedPlacesFraction = reports.mapNotNull {
-                                    val upToDateElements = if (it.tags.optLong("up_to_date_elements", -1) == -1L) {
-                                        return@mapNotNull null
-                                    } else {
-                                        it.tags.getLong("up_to_date_elements")
-                                    }
+                                    val upToDateElements =
+                                        if (it.tags.optLong("up_to_date_elements", -1) == -1L) {
+                                            return@mapNotNull null
+                                        } else {
+                                            it.tags.getLong("up_to_date_elements")
+                                        }
 
-                                    val totalElements = if (it.tags.optLong("total_elements", -1) == -1L) {
-                                        return@mapNotNull null
-                                    } else {
-                                        it.tags.getLong("total_elements")
-                                    }
+                                    val totalElements =
+                                        if (it.tags.optLong("total_elements", -1) == -1L) {
+                                            return@mapNotNull null
+                                        } else {
+                                            it.tags.getLong("total_elements")
+                                        }
 
                                     Pair(
                                         first = it.date.toString(),
                                         second = upToDateElements.toFloat() / totalElements.toFloat() * 100f
                                     )
                                 },
-                                daysSinceVerified = reports.map {
+                                daysSinceVerified = reports.mapNotNull {
                                     val avgVerificationDate =
                                         it.tags.optString("avg_verification_date")
+                                            .ifBlank { return@mapNotNull null }
                                     Pair(
                                         first = it.date,
                                         second = Duration.between(
