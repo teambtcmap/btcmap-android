108f7a7bb6cc00943b993f7f717bbac50f864627
Igor Bubelov <igor@bubelov.com>
2025-01-22 15:29:28 +0700

    Generate donation QRs on the fly

--- app/src/main/kotlin/donation/DonationFragment.kt
+++ app/src/main/kotlin/donation/DonationFragment.kt
@@ -4,13 +4,21 @@ import android.content.ClipData
 import android.content.ClipboardManager
 import android.content.Context
 import android.content.Intent
+import android.graphics.Color
 import android.net.Uri
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidmads.library.qrgenearator.QRGContents
+import androidmads.library.qrgenearator.QRGEncoder
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
+import kotlinx.coroutines.launch
 import org.btcmap.R
 import org.btcmap.databinding.FragmentDonationBinding
 
@@ -29,42 +37,14 @@ class DonationFragment : Fragment() {
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        binding.apply {
-            topAppBar.setNavigationOnClickListener { parentFragmentManager.popBackStack() }
-            listOf(qr, pay).forEach {
-                it.setOnClickListener {
-                    val intent = Intent(Intent.ACTION_VIEW)
-                    intent.data =
-                        Uri.parse("bitcoin:${getString(R.string.donation_address_onchain)}")
-                    runCatching {
-                        startActivity(intent)
-                    }.onFailure {
-                        Toast.makeText(
-                            requireContext(),
-                            R.string.you_dont_have_a_compatible_wallet,
-                            Toast.LENGTH_LONG
-                        ).show()
-                    }
-                }
+        super.onViewCreated(view, savedInstanceState)
+
+        viewLifecycleOwner.lifecycleScope.launch {
+            repeatOnLifecycle(Lifecycle.State.STARTED) {
+                binding.topAppBar.setNavigationOnClickListener { parentFragmentManager.popBackStack() }
+                initOnchain()
+                initLightning()
             }
-            copy.setOnClickListener { onCopyButtonClick() }
-            listOf(lnQr, lnPay).forEach {
-                it.setOnClickListener {
-                    val intent = Intent(Intent.ACTION_VIEW)
-                    intent.data =
-                        Uri.parse("lightning:${getString(R.string.donation_address_lightning)}")
-                    runCatching {
-                        startActivity(intent)
-                    }.onFailure {
-                        Toast.makeText(
-                            requireContext(),
-                            R.string.you_dont_have_a_compatible_wallet,
-                            Toast.LENGTH_LONG
-                        ).show()
-                    }
-                }
-            }
-            lnCopy.setOnClickListener { onLnCopyButtonClick() }
         }
     }
 
@@ -73,21 +53,78 @@ class DonationFragment : Fragment() {
         _binding = null
     }
 
-    private fun onCopyButtonClick() {
-        val clipManager =
-            requireContext().getSystemService(Context.CLIPBOARD_SERVICE) as ClipboardManager
-        val clipLabel = getString(R.string.btc_map_donation_address)
-        val clipText = getString(R.string.donation_address_onchain)
-        clipManager.setPrimaryClip(ClipData.newPlainText(clipLabel, clipText))
-        Toast.makeText(requireContext(), R.string.copied_to_clipboard, Toast.LENGTH_SHORT).show()
+    private fun initOnchain() {
+        val paymentAddress = getString(R.string.donation_address_onchain)
+        val paymentUrl = "bitcoin:$paymentAddress"
+
+        val qrEncoder = QRGEncoder(paymentUrl, null, QRGContents.Type.TEXT, 1000)
+        qrEncoder.colorBlack = Color.BLACK
+        qrEncoder.colorWhite = Color.WHITE
+        val bitmap = qrEncoder.getBitmap(0)
+        binding.onchainQr.setImageBitmap(bitmap)
+
+        listOf(binding.onchainQr, binding.onchainPay).forEach {
+            it.setOnClickListener { payWithExternalWallet(paymentUrl) }
+        }
+
+        binding.onchainCopyAddress.setOnClickListener {
+            addToClipboard(
+                label = getString(R.string.btc_map_donation_address),
+                text = paymentAddress,
+            )
+        }
     }
 
-    private fun onLnCopyButtonClick() {
+    private fun initLightning() {
+        val paymentAddress = getString(R.string.donation_address_lightning)
+        val paymentUrl = "lightning:$paymentAddress"
+
+        val qrEncoder = QRGEncoder(paymentUrl, null, QRGContents.Type.TEXT, 1000)
+        qrEncoder.colorBlack = Color.BLACK
+        qrEncoder.colorWhite = Color.WHITE
+        val bitmap = qrEncoder.getBitmap(0)
+        binding.lnQr.setImageBitmap(bitmap)
+
+        listOf(binding.lnQr, binding.lnPay).forEach {
+            it.setOnClickListener { payWithExternalWallet(paymentUrl) }
+        }
+
+        binding.lnCopyAddress.setOnClickListener {
+            addToClipboard(
+                label = getString(R.string.btc_map_donation_address),
+                text = paymentAddress,
+            )
+        }
+    }
+
+    private fun payWithExternalWallet(paymentUrl: String) {
+        runCatching {
+            require(paymentUrl.startsWith("bitcoin") || paymentUrl.startsWith("lightning"))
+            Uri.parse(paymentUrl)
+        }.onFailure {
+            MaterialAlertDialogBuilder(requireContext())
+                .setTitle(R.string.error)
+                .setMessage(R.string.invalid_payment_url)
+                .setPositiveButton(R.string.close, null)
+                .show()
+            return
+        }
+
+        runCatching {
+            startActivity(Intent(Intent.ACTION_VIEW, Uri.parse(paymentUrl)))
+        }.onFailure {
+            MaterialAlertDialogBuilder(requireContext())
+                .setTitle(R.string.error)
+                .setMessage(R.string.you_dont_have_a_compatible_wallet)
+                .setPositiveButton(R.string.close, null)
+                .show()
+        }
+    }
+
+    private fun addToClipboard(label: String, text: String) {
         val clipManager =
             requireContext().getSystemService(Context.CLIPBOARD_SERVICE) as ClipboardManager
-        val clipLabel = getString(R.string.btc_map_donation_address)
-        val clipText = getString(R.string.donation_address_lightning)
-        clipManager.setPrimaryClip(ClipData.newPlainText(clipLabel, clipText))
+        clipManager.setPrimaryClip(ClipData.newPlainText(label, text))
         Toast.makeText(requireContext(), R.string.copied_to_clipboard, Toast.LENGTH_SHORT).show()
     }
 }
