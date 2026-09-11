17dd4454616d89b3da12bef16f40d62e58ca0287
Igor Bubelov <igor@bubelov.com>
2026-09-11 12:24:15 +0700

    Update Kotlin to 2.4.20

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,9 @@
+buildscript {
+    dependencies {
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:${libs.versions.kotlin.get()}")
+    }
+}
+
 plugins {
     alias(libs.plugins.android.application) apply false
 }
